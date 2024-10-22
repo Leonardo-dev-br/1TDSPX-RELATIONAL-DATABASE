3d54a9a1ff147900544a46c006a1ee01025862b1
Leonardo Pimentel <99846550+Leonardo-dev-br@users.noreply.github.com>
2024-10-21 22:24:43 -0300

    WIP: Inserindo Valores

---
+++ dml.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_endereco (
+    nome_rua,
+    numero,
+    cep,
+    bairro,
+    cidade,
+    pais,
+    complemento
+) VALUES (
+    'PRAÇA DA SÉ',
+    1 ,
+    '01001-000',
+    'SÉ',
+    'SAO PAULO',
+    'BRASIL',
+    'CATEDRAL METROPOLITANA '
+);
